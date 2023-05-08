a0b2fcdf409d30e2585846be6a002d3cfcaadba5
Lexus39 <iaitel1488@gmail.com>
2023-05-08 23:20:39 +0300

    Добавить UserService в InternshipTest.Application.

---
+++ src/InternshipTest.Application/Services/UserService.cs
@@ -0,0 +1,55 @@
+﻿using InternshipTest.Application.Interfaces;
+using InternshipTest.Domain.UserAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipTest.Application.Services
+{
+    public class UserService
+    {
+        private IUserRepository _userRepository = null!;
+
+        public UserService(IUserRepository userRepository) 
+        { 
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            var user = await _userRepository.GetUserByIdAsync(id);
+            if (user.UserState.Code != "Active")
+                throw new ArgumentException();
+            return user;
+        }
+
+        public async Task<User> GetUserByLoginAsync(string login)
+        {
+            var user = await _userRepository.GetUserByLoginAsync(login);
+            if (user.UserState.Code != "Active")
+                throw new ArgumentException();
+            return user;
+        }
+
+        public async Task<List<User>> GetUsersAsync() 
+            => await _userRepository.GetAllActiveUsersAsync();
+
+        public async Task<User> UpdateUserAsync(UserUpdateParameters userUpdateParameters)
+        {
+            var user = await GetUserByIdAsync(userUpdateParameters.Id);
+            var updatedUser = User.UpdateUser(userUpdateParameters);
+            if (user.Login != updatedUser.Login 
+                || user.CreatedDate != updatedUser.CreatedDate)
+                throw new ArgumentException();
+            return await _userRepository.UpdateUserAsync(user);
+        }
+
+        public async Task<User> CreateUser(UserCreateParameters userCreateParameters)
+        {
+            var user = User.CreateUser(userCreateParameters);
+            return await _userRepository.AddUserAsync(user);
+        }
+    }
+}
