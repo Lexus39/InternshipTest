4bed3f98e3e13dfd91d1b99ebb4772f7be80894b
Lexus39 <iaitel1488@gmail.com>
2023-05-08 23:19:46 +0300

    Добавить интерфейс IUserRepository в InternshipTest.Application

---
+++ src/InternshipTest.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using InternshipTest.Domain.UserAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipTest.Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Task<User> GetUserByIdAsync(int id);
+        public Task<User> GetUserByLoginAsync(string login);
+        public Task<List<User>> GetAllActiveUsersAsync();
+        public Task<User> UpdateUserAsync(User user);
+        public Task<User> AddUserAsync(User user);
+    }
+}
