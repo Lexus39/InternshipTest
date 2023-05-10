628bec11103435f957157f1b2316552d59650287
Lexus39 <iaitel1488@gmail.com>
2023-05-10 22:47:02 +0300

    Добавить AutoMapper и MappingProfile в проект InternshipTest.Api

---
+++ src/InternshipTest.Api/Mappings/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using InternshipTest.Domain.UserAggregate;
+using IntertnshipTest.DAL.Entities;
+
+namespace InternshipTest.API.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserGroupEntity, UserGroup>().ReverseMap();
+            CreateMap<UserStateEntity, UserState>().ReverseMap();
+            CreateMap<UserEntity, User>();
+        }
+    }
+}
