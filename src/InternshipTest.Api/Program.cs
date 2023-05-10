cb3f77b20c119fa123b2fe2aedd12f675ee6b16a
Lexus39 <iaitel1488@gmail.com>
2023-05-10 22:52:45 +0300

    Сконфигурировать InternshipTest.Api

--- src/InternshipTest.Api/Program.cs
+++ src/InternshipTest.Api/Program.cs
@@ -1,3 +1,13 @@
+using InternshipTest.API.Mappings;
+using InternshipTest.API.Middleware;
+using InternshipTest.Application.Interfaces;
+using InternshipTest.Application.Services;
+using IntertnshipTest.DAL;
+using IntertnshipTest.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.Xml;
+using System.Runtime.CompilerServices;
+
 namespace InternshipTest.Api
 {
     public class Program
@@ -12,6 +22,29 @@ namespace InternshipTest.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.ConfigureSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Version= "v1",
+                    Title = "Internship test API",
+                    Description = "API, которое позволяет добавлять, получать, изменять и удалять пользователей"
+                });
+                var basePath = Directory.GetCurrentDirectory();
+                var path = Path.Combine(basePath, @"obj\Debug\net6.0\InternshipTest.API.xml");
+                options.IncludeXmlComments(path);
+            });
+
+            var connectionString =  builder.Configuration.GetConnectionString("Default");
+
+            builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+            builder.Services.AddDbContext<InternshipContext>(options =>
+             options.UseNpgsql(connectionString));
+
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+            builder.Services.AddScoped<UserService>();
 
             var app = builder.Build();
 
@@ -22,10 +55,7 @@ namespace InternshipTest.Api
                 app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
+            app.UseCustomExceptionHandler();
 
             app.MapControllers();
 
